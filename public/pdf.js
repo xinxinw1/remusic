690de86caf155f2e9ddfb949dad78ab276dd9557
Xin-Xin Wang - blueberry <get@xin-xin.me>
2018-09-15 16:16:04 -0400

    start dialog update

--- public/pdf.js
+++ public/pdf.js
@@ -8,6 +8,33 @@ var pdfjsLib = window['pdfjs-dist/build/pdf'];
 // The workerSrc property shall be specified.
 pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://mozilla.github.io/pdf.js/build/pdf.worker.js';
 
+function setDialogPosition(dialogElement, startEvent, currEvent) {
+  var height = Math.abs(startEvent.pageY - currEvent.pageY);
+  var width = Math.abs(startEvent.pageX - currEvent.pageX);
+  var my;
+  if (startEvent.pageX <= currEvent.pageX) {
+    if (startEvent.pageY <= currEvent.pageY) {
+      my = "left top";
+    } else {
+      // startY > currY
+      my = "left bottom";
+    }
+  } else {
+    // startX > currX
+    if (startEvent.pageY <= currEvent.pageY) {
+      my = "right top";
+    } else {
+      my = "right bottom";
+    }
+  }
+
+  console.log(height, width);
+
+  //dialogElement.dialog("option", "position", { my: my, at: "center", of: startEvent });
+  //dialogElement.dialog("option", "width", width);
+  //dialogElement.dialog("option", "height", 200);
+}
+
 // Asynchronous download of PDF
 var loadingTask = pdfjsLib.getDocument(url);
 loadingTask.promise.then(function(pdf) {
@@ -26,6 +53,34 @@ loadingTask.promise.then(function(pdf) {
     var context = canvas.getContext('2d');
     canvas.height = viewport.height;
     canvas.width = viewport.width;
+    var mousedownEvent = null;
+    var down = false;
+    var dialog = $("#dialog-highlight");
+    $(canvas).on("mousedown", function (e) {
+      console.log("down", e);
+      e.preventDefault();
+      mousedownEvent = e;
+      down = true;
+      dialog.dialog({
+        dialogClass: "highlight",
+        minHeight: 1,
+        minWidth: 1,
+        height: 10,
+        width: 200,
+        position: {my: "left top", at: "center", of: mousedownEvent}
+      });
+    });
+    $(document).on("mouseup", function (e) {
+      console.log("up", e);
+      down = false;
+    });
+    $(canvas).on("mousemove", function (e) {
+      if (down) {
+        e.preventDefault();
+        console.log("mouse move ",  e);
+        setDialogPosition(dialog, mousedownEvent, e);
+      }
+    });
 
     // Render PDF page into canvas context
     var renderContext = {
@@ -36,13 +91,6 @@ loadingTask.promise.then(function(pdf) {
     renderTask.then(function () {
       console.log('Page rendered');
       $(function () {
-        $("#dialog-highlight").dialog({
-          dialogClass: "highlight",
-          minHeight: 1,
-          minWidth: 1,
-          height: 100,
-          width: 100
-        });
         $("#dialog-comment").dialog({
           title: "Comment by Xin-Xin",
           position: {my: "left center", at: "right center", of: "#the-canvas"}
