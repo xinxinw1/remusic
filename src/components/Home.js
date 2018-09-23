e00f053d2d288d83a925729d4730dad9add62630
Xin-Xin Wang - blueberry <get@xin-xin.me>
2018-09-23 15:56:23 -0400

    restructure

---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Home = () => (
+  <div>
+    <h1>Welcome to Re:Music</h1>
+  </div>
+)
+
+export default Home
--- src/index.js
+++ src/index.js
@@ -1,38 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Link, Switch, Route, BrowserRouter } from "react-router-dom";
-
-const Main = () => (
-  <main>
-    <Switch>
-      <Route exact path='/' component={Home} />
-    </Switch>
-  </main>
-)
-
-const App = () => (
-  <div>
-    <Header />
-    <Main />
-  </div>
-)
-
-const Home = () => (
-  <div>
-    <h1>Welcome to Re:Music</h1>
-  </div>
-)
-
-const Header = () => (
-  <header>
-    <nav>
-      <ul>
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/what'>What</Link></li>
-      </ul>
-    </nav>
-  </header>
-)
+import { BrowserRouter } from "react-router-dom";
+import App from "./components/App"
 
 ReactDOM.render((
   <BrowserRouter>
