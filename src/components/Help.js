63e8bf87d62c3768abe8c6e0426c64cdc94e1620
Xin-Xin Wang - blueberry <get@xin-xin.me>
2018-09-30 17:06:56 -0400

    remove font-awesome from css

--- src/components/Help.js
+++ src/components/Help.js
@@ -1,30 +1,25 @@
 import React from "react";
-import "../css/font-awesome-4.7.0/css/font-awesome.min.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
-export default class Help extends React.Component {
+import vextab from 'vextab/releases/vextab-div';
+
+class Help extends React.Component {
     constructor(props) {
         super(props);
     }
 
-    componentDidMount () {
-        const script1 = document.createElement("script");
-        script1.src = "vextab-div.js"
-        script1.async = true;
-
-        document.body.appendChild(script1);
-    }
-
     render(){
         return (
-            <body>
-            <div className="vex-tabdiv" width="680" scale="1.0" editor="true" editor_width="680" editor_height="110" style="position: relative;">stave clef=treble key=Bb time=4/4
+          <div>
+            <FontAwesomeIcon icon={faCoffee} />
+            <div className="vex-tabdiv" width="680" scale="1.0" editor="true" editor_width="680" editor_height="110" style={{position: "relative"}}>stave clef=treble key=Bb time=4/4
                 notes :4 (Cn/4.E#/4.G@/4) $.a./bottom.$ $Hi$
                 notes :16 E##-F@@-G-A/5 $.a-/top.,..,.a>/top.,..$ $Team,Xin-Xin,Sophie,Zack$
                 notes :8 DTF-A/4 ^3^ $.a@u/bottom.$ :8d B/4 $.a@a/top.$ :16 ## | :4 A/4 T A/4
             </div>
             <p id="help">See <a href="http://my.vexflow.com/articles/134">this link</a> for instructions on how to use Music Editor</p>
-            </body>
-
+          </div>
         );
     }
 }
