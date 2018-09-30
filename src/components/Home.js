6da99d8c2272f3c5841d6fa10dbc55422a0c6aca
thereclaim3r <zyew@edu.uwaterloo.ca>
2018-09-30 14:47:23 -0400

    Homepage done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,31 @@
 import React from "react";
-import { Jumbotron, Button } from 'reactstrap';
+import { Jumbotron, Container, Table } from 'reactstrap';
+import "../css/main-page.css";
 
 const Home = () => (
-    <Jumbotron className={"center"}>
-        <div>
-            <h1>Welcome to Re:Music</h1>
-        </div>
-    </Jumbotron>
+    <div>
+        <Jumbotron fluid className="jumbo_colour">
+            <Container fluid className="center">
+                <h1 className="display-3">Welcome to Re:Music</h1>
+                <h3>Easy Online Music Reviewing</h3>
+                <hr/>
+                <p className="lead">Share | Collaborate | Innovate</p>
+            </Container>
+        </Jumbotron>
+        <Table dark >
+            <thead>
+                <th>Hi Sophie and XinXin</th>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Zack here btw</td>
+                </tr>
+                <tr>
+                    <td>How are you guys doing?</td>
+                </tr>
+            </tbody>
+        </Table>
+    </div>
 );
 
 export default Home
