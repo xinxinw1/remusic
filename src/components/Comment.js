d58e0f53f01f6e3b2ce7ce1f8dec0f8412080503
Xin-Xin Wang - blueberry <get@xin-xin.me>
2018-10-28 22:43:05 -0400

    refactor vextab, add new comment

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,26 +1,89 @@
 import React from "react";
 import "./Comment.css";
 
+import { VexTab } from './VexTab';
 import { Card, CardText, CardBody, CardTitle, Button } from 'reactstrap';
+
+const defaultMusic = `stave clef=treble key=Bb time=4/4
+notes :4 A/4 B/4 C/4 D/4`;
 
 class CommentChain extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      inserting: false,
       type: "",
       content: ""
     };
+    this.onMusicValueChange = this.onMusicValueChange.bind(this);
+    this.onTextValueChange = this.onTextValueChange.bind(this);
   }
 
-  startInsert(type) {
+  startInsert(e, type) {
+    e.preventDefault();
+    let defaultValue = type == "music" ? defaultMusic : "";
     this.setState({
-      inserting: true,
-      type: type
+      type: type,
+      content: defaultValue
     });
   }
 
+  onMusicValueChange(newValue) {
+    this.setState({
+      content: newValue
+    });
+  }
+
+  onTextValueChange(e) {
+    this.setState({
+      content: e.target.value
+    });
+  }
+
+  submitComment(e) {
+
+  }
+
+  cancelComment(e) {
+
+  }
+
   render() {
+    let buttons = (
+      <React.Fragment>
+        <Button onClick={e => this.startInsert(e, "music")}>+ Music</Button>{' '}
+        <Button onClick={e => this.startInsert(e, "text")}>+ Text</Button>
+      </React.Fragment>
+    );
+    if (this.state.type) {
+      buttons = (
+        <React.Fragment>
+          <Button onClick={this.submitComment}>Submit</Button>{' '}
+          <Button onClick={this.cancelComment}>Cancel</Button>
+        </React.Fragment>
+      );
+    }
+    let insertBox = null;
+    if (this.state.type == "music") {
+      insertBox = (
+        <VexTab
+          width={250}
+          editorHeight={80}
+          value={this.state.content}
+          onValueChange={this.onMusicValueChange}
+        />
+      )
+    } else if (this.state.type == "text") {
+      insertBox = (
+        <p>
+        <textarea
+          style={{width: 250, height: 80}}
+          value={this.state.content}
+          onChange={this.onTextValueChange}
+        />
+        </p>
+      )
+    }
+
     return (
       <Card
         className="comment-chain"
@@ -28,8 +91,8 @@ class CommentChain extends React.Component {
       >
         <CardBody>
           {this.props.children}
-          <Button onClick={e => this.startInsert("music")}>+ Music</Button>{' '}
-          <Button onClick={e => this.startInsert("text")}>+ Text</Button>
+          {insertBox}
+          {buttons}
         </CardBody>
       </Card>
     )
--- src/components/Help.js
+++ src/components/Help.js
@@ -2,7 +2,7 @@ import React from "react";
 import {Container} from 'reactstrap'
 import "./Help.css";
 
-import VexTab from './VexTab';
+import { VexTabStateful } from './VexTab';
 
 let defaultContent = `stave clef=treble key=Bb time=4/4
 notes :4 (Cn/4.E#/4.G@/4) $.a./bottom.$ $Hi$
@@ -11,7 +11,7 @@ notes :8 DTF-A/4 ^3^ $.a@u/bottom.$ :8d B/4 $.a@a/top.$ :16 ## | :4 A/4 T A/4`;
 
 const Help = () => (
   <Container fluid className="center-help">
-    <VexTab value={defaultContent} />
+    <VexTabStateful initValue={defaultContent} />
     <p id="help">See <a href="http://my.vexflow.com/articles/134">this link</a> for instructions on how to use Music Editor</p>
   </Container>
 );
